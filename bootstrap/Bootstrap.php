a212fda22c89b14e290604b14e187a96a234a141
jeemu <zhoulin1359@qq.com>
2017-11-15 00:31:36 +0800

    异常处理

--- bootstrap/Bootstrap.php
+++ bootstrap/Bootstrap.php
@@ -14,7 +14,8 @@ class Bootstrap extends Yaf\Bootstrap_Abstract
     /**
      * 初始化项目配置
      */
-    public function _initSetting(){
+    public function _initSetting()
+    {
         Yaf\Dispatcher::getInstance()->disableView();   //关闭自动调用引擎render方法；
 
         Yaf\Loader::getInstance()->registerLocalNamespace(array("Db", "Bar"));  //注册本地类前缀
@@ -23,14 +24,15 @@ class Bootstrap extends Yaf\Bootstrap_Abstract
     /**
      * 初始化路由
      */
-    public function _initRouter(Yaf\Dispatcher $dispatcher){
-        $router =  $dispatcher::getInstance()->getRouter();
+    public function _initRouter(Yaf\Dispatcher $dispatcher)
+    {
+        $router = $dispatcher::getInstance()->getRouter();
         $routeConf = [
-            'simple'=>[
-                'type'=>'simple',
-                'controller'=>'c',
-                'module'=>'m',
-                'action'=>'a'
+            'simple' => [
+                'type' => 'simple',
+                'controller' => 'c',
+                'module' => 'm',
+                'action' => 'a'
             ]
         ];
         $router->addConfig($routeConf);
@@ -39,19 +41,21 @@ class Bootstrap extends Yaf\Bootstrap_Abstract
     /**
      * 自定义文件加载；yaf已经实现自动加载，扩充
      */
-    public function _initAutoload(){
-        Yaf\loader::import(APP_PATH.'/vendor/autoload.php'); //composer
-        Yaf\loader::import(APP_PATH.'/bootstrap/common.php'); //自定义方法
-       //spl_autoload_register('Bootstrap::userAutoload');
+    public function _initAutoload()
+    {
+        Yaf\loader::import(APP_PATH . '/vendor/autoload.php'); //composer
+        Yaf\loader::import(APP_PATH . '/bootstrap/common.php'); //自定义方法
+        //spl_autoload_register('Bootstrap::userAutoload');
     }
 
     /**
      * 自定义加载具体实现
      * @param $class 类名
      */
-    public static function userAutoload($class){
-        $file = APP_PATH.'/library/Autoload/'.$class.'.php';
-        if (is_file($file)){
+    public static function userAutoload($class)
+    {
+        $file = APP_PATH . '/library/Autoload/' . $class . '.php';
+        if (is_file($file)) {
             Yaf\loader::import($file);  //可以使用 require_once，但Yaf_loader::import()效率更高
         }
     }
@@ -61,13 +65,27 @@ class Bootstrap extends Yaf\Bootstrap_Abstract
      * 加载配置项；读取配置文件保存到对象注册表中
      *
      */
-    public function _initConfig(){
-        $configFile = APP_PATH.'/conf/config.php';
-        if (is_file($configFile)){
+    public function _initConfig()
+    {
+        $configFile = APP_PATH . '/conf/config.php';
+        if (is_file($configFile)) {
             $config = include($configFile);
-            Yaf\Registry::set('config',$config);
+            Yaf\Registry::set('config', $config);
         }
     }
 
+
+    public function _initErrorHandle(Yaf\Dispatcher $dispatcher)
+    {
+        $dispatcher::getInstance()->throwException(true);
+        set_error_handler(function ($errNo, $errStr, $errFile, $errLine) {
+            Jeemu\Log::getInstance()->write('捕捉异常:'.$errNo.'|'.$errStr.'|'.$errFile.'|'.$errLine,'ERROR');
+            echo(2);
+        });
+        set_exception_handler(function ($ex){
+            Jeemu\Log::getInstance()->write('捕捉异常:'.var_export($ex,true),'ERROR');
+            echo(1112);
+        });
+    }
 
 }
